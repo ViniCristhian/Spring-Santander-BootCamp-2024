096a04f10ddafd9b6d650b88375aae5c70d7d256
ViniciusCris <143042392+Vini14cius09@users.noreply.github.com>
2024-07-27 22:06:47 -0300

    Committ do projeto para o github

---
+++ src/test/java/com/dio/santander/bootcamp/BootCampSantader2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCampSantader2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
